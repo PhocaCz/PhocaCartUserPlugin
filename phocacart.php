4421edf136d0a4a98217685d43c5c837215864b7
Jan <PhocaCz@users.noreply.github.com>
2022-02-23 21:08:05 +0100

    4.0.0

--- phocacart.php
+++ phocacart.php
@@ -9,11 +9,14 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
  */
 
+use Joomla\CMS\Factory;
+use Joomla\CMS\Mail\MailHelper;
+
 defined('_JEXEC') or die;
 
 class PlgUserPhocacart extends JPlugin
 {
-	
+
 	public function onUserAfterSave($user, $isnew, $success, $msg)
 	{
 
@@ -21,14 +24,14 @@ class PlgUserPhocacart extends JPlugin
 		$user_shipping_address = $this->params->get('user_shipping_address', 0);
 		$order_billing_address = $this->params->get('order_billing_address', 0);
 		$order_shipping_address = $this->params->get('order_shipping_address', 0);
-	
-	
-	
-	
+
+
+
+
 		if (isset($user['id']) && (int)$user['id'] > 0 && isset($user['email'])) {
-			
-			$db = JFactory::getDBO();
-			
+
+			$db = Factory::getDBO();
+
 			// BILLING ADDRESS
 			$query = ' SELECT id, email FROM #__phocacart_users AS a'
 			    .' WHERE a.user_id = '.(int) $user['id']
@@ -37,9 +40,9 @@ class PlgUserPhocacart extends JPlugin
 
 			$db->setQuery($query);
 			$userPc = $db->loadAssoc();
-			
+
 			if (isset($userPc['email']) && $userPc['email'] != '') {
-				if ($userPc['email'] != $user['email'] && JMailHelper::isEmailAddress($user['email'])) {
+				if ($userPc['email'] != $user['email'] && MailHelper::isEmailAddress($user['email'])) {
 					// Change email in Phoca Cart User table
 					if ($user_billing_address == 1) {
 						$query = 'UPDATE #__phocacart_users SET'
@@ -47,7 +50,7 @@ class PlgUserPhocacart extends JPlugin
 								. ' WHERE user_id = ' . (int) $user['id']
 								. ' AND type = 0';
 						$db->setQuery($query);
-						$db->execute();	
+						$db->execute();
 					}
 					// Change emails in existing orders
 					if ($order_billing_address == 1 && isset($userPc['id']) && (int)$userPc['id'] > 0) {
@@ -59,10 +62,10 @@ class PlgUserPhocacart extends JPlugin
 					}
 				}
 			}
-			
-			
+
+
 			$userPc = array();
-			
+
 			// SHIPPING ADDRESS
 			$query = ' SELECT id, email FROM #__phocacart_users AS a'
 			    .' WHERE a.user_id = '.(int) $user['id']
@@ -71,9 +74,9 @@ class PlgUserPhocacart extends JPlugin
 
 			$db->setQuery($query);
 			$userPc = $db->loadAssoc();
-			
+
 			if (isset($userPc['email']) && $userPc['email'] != '') {
-				if ($userPc['email'] != $user['email'] && JMailHelper::isEmailAddress($user['email'])) {
+				if ($userPc['email'] != $user['email'] && MailHelper::isEmailAddress($user['email'])) {
 					// Change email in Phoca Cart User table
 					if ($user_shipping_address == 1) {
 						$query = 'UPDATE #__phocacart_users SET'
